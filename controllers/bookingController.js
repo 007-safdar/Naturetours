a0fcf05e6cf66760728c1a2327d34332365ee4c5
xx172 <zeinsafdar@gmail.com>
2021-12-16 01:25:09 +0530

    dev

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -47,7 +47,7 @@ exports.getCheckoutSession=catchAsync(async(req,res,next)=>{
 const createBookingCheckout =async session=>{
     const tour=session.client_reference_id;
     const user=(await User.findOne({email:session.customer_email})).id;
-    const price=session.line_items[0].amount/100;
+    const price=session.display_items[0].amount/100;
     await Boooking.create({tour , user, price});
 }
 exports.webhookCheckout=(req,res,next)=>{
@@ -62,7 +62,7 @@ exports.webhookCheckout=(req,res,next)=>{
         return res.status(400).send(`Webhook error:${err.message}`)
     }
 
-    if(event.type==='checkout.session.complete'){
+    if(event.type==='checkout.session.completed'){
         createBookingCheckout(event.data.object);
         res.status(200).json({recieved:true});
     }
