4e55ce3b8b827ee38b5e489815dea7baf41a56f1
xx172 <zeinsafdar@gmail.com>
2021-12-16 01:18:18 +0530

    dev

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -4,7 +4,8 @@ const authController = require('../controllers/authController');
 const bookingController = require('../controllers/bookingController');
 const router = express.Router();
 
-router.get('/',bookingController.createBookingCheckOut, authController.isLoggedIn, viewsController.getOverview);
+router.get('/',
+ authController.isLoggedIn, viewsController.getOverview);
 router.get('/tour/:slug', authController.isLoggedIn, viewsController.getTour);
 router.get('/login', authController.isLoggedIn, viewsController.getLoginForm);
 router.get('/me', authController.protect, viewsController.getAccount);
