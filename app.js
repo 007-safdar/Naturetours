9ccdb1552f07a64a559af271f2717f721362e242
xx172 <zeinsafdar@gmail.com>
2021-12-16 01:11:32 +0530

    dev

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const bookingRouter = require('./routes/bookingRoutes');
+const bookingController = require('./controllers/bookingController');
 const reviewRouter = require('./routes/reviewRoutes');
 const viewRouter = require('./routes/viewRoutes');
 
@@ -45,6 +46,7 @@ const limiter = rateLimit({
 });
 app.use('/api', limiter);
 
+app.post('/webhook-checkout',express.raw({type:application.json}),bookingController.webhookCheckout);
 // Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
 app.use(express.urlencoded({ extended: true, limit: '10kb' }));
