08b5347a71f7d5edcd2c58a8769a4740269d77e4
xx172 <zeinsafdar@gmail.com>
2021-12-16 01:40:57 +0530

    dev

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -65,6 +65,7 @@ exports.webhookCheckout=(req,res,next)=>{
 
     if(event.type==='checkout.session.completed'){
         createBookingCheckout(event.data.object);
-        res.status(200).json({recieved:true});
+       
     }
+    res.status(200).json({recieved:true});
 };
