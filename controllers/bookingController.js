e9738b9c537f8797494ef90b059240aeb791e2ac
xx172 <zeinsafdar@gmail.com>
2021-12-16 02:04:17 +0530

    dev

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -37,7 +37,7 @@ exports.getCheckoutSession=catchAsync(async(req,res,next)=>{
     });
 });
 const createBookingCheckout =async session=>{
-    await Boooking.create({tour , user, price});
+    await Booking.create({tour , user, price});
     const tour=session.client_reference_id;
     const user=(await User.findOne({email:session.customer_email})).id;
     const price=session.line_items[0].amount/100;
